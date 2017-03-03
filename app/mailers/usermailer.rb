17e3b886b575f1e8b0043251150f382306f429a0
mahesh2012 <jmaheshkumar2@gmail.com>
2017-03-03 13:24:26 +0530

    Initial Commit

---
+++ app/mailers/usermailer.rb
@@ -0,0 +1,13 @@
+class Usermailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.usermailer.livechat.subject
+  #
+  def livechat(user)
+    @user = "user.email"
+
+    mail to: "saiandalindia@gmail.com"
+  end
+end
